180c5ea295c7ada23ac777361401f23a31f2be00
benny <b1623546466@gmail.com>
2023-12-21 02:48:39 +0800

    save

--- mongodb/compare.py
+++ mongodb/compare.py
@@ -5,6 +5,16 @@ from deepdiff import DeepDiff
 from loguru import logger
 
 
+exit_code = 0
+
+
+def log(msg: str, obj):
+    global exit_code
+    if obj:
+        logger.info(f'{msg}: {obj}')
+        exit_code += 1
+
+
 def size_compare(source: dict, target: dict, gap: int):
     new_source = {}
     new_target = {}
@@ -18,6 +28,8 @@ def size_compare(source: dict, target: dict, gap: int):
 
 
 def compare_db(source: MongoClient, target: MongoClient, ignored_db: list[str] = None):
+    global exit_code
+
     source_dbs = list(filter(lambda item: item['name'] not in ignored_db, source.list_databases()))
     target_dbs = list(filter(lambda item: item['name'] not in ignored_db, target.list_databases()))
     logger.debug(f'source dbs {source_dbs}')
@@ -26,26 +38,24 @@ def compare_db(source: MongoClient, target: MongoClient, ignored_db: list[str] =
         sorted([i['name'] for i in source_dbs]),
         sorted([i['name'] for i in target_dbs]),
     )
-    if db_diff:
-        logger.info(f'db-diff: {db_diff}')
+    log('db-diff', db_diff)
 
     size_diff = DeepDiff(*size_compare(
         dict([(i['name'], i['sizeOnDisk']) for i in source_dbs]),
         dict([(i['name'], i['sizeOnDisk']) for i in target_dbs]),
         1024*1024
     ))
-    if size_diff:
-        logger.info(f'db-size-diff: {size_diff}')
+    log('db-size-diff', size_diff)
 
 
 def compare_collection(source: Database, target: Database):
+    global exit_code
     source_collections = sorted(source.list_collection_names())
     target_collections = sorted(target.list_collection_names())
     collection_diff = DeepDiff(source_collections, target_collections)
     logger.debug(f'source collections {source_collections}')
 
-    if collection_diff:
-        logger.info(f'collection-diff: {collection_diff}')
+    log('collection-diff', collection_diff)
 
     source_status = dict([(i, source.command('collStats', i)) for i in source_collections])
     target_status = dict([(i, source.command('collStats', i)) for i in target_collections])
@@ -55,16 +65,14 @@ def compare_collection(source: Database, target: Database):
         dict([(k, v['size']) for k, v in target_status.items()]),
         1024 * 1024
     ))
-    if collection_size_diff:
-        logger.info(f'collection-size-diff: {collection_size_diff}')
+    log('collection-size-diff', collection_size_diff)
 
     collection_count_diff = DeepDiff(*size_compare(
         dict([(k, v['count']) for k, v in source_status.items()]),
         dict([(k, v['count']) for k, v in target_status.items()]),
         100
     ))
-    if collection_count_diff:
-        logger.info(f'collection-count-diff: {collection_count_diff}')
+    log('collection-count-diff', collection_count_diff)
 
     # compare indices
     collection_index_size_diff = DeepDiff(*size_compare(
@@ -72,15 +80,13 @@ def compare_collection(source: Database, target: Database):
         dict([(k, v['totalIndexSize']) for k, v in target_status.items()]),
         1024*1024
     ))
-    if collection_index_size_diff:
-        logger.info(f'collection-totalIndexSize-diff: {collection_index_size_diff}')
+    log('collection-totalIndexSize-diff', collection_index_size_diff)
 
     collection_index_diff = DeepDiff(
         dict([(k, v['indexSizes'].keys()) for k, v in source_status.items()]),
         dict([(k, v['indexSizes'].keys()) for k, v in target_status.items()]),
     )
-    if collection_index_diff:
-        logger.info(f'collection-index-diff: {collection_index_diff}')
+    log('collection-index-diff', collection_index_diff)
 
     # TODO index diff of each indices
 
@@ -100,6 +106,8 @@ def start(source: str, target: str, db: str):
             compare_collection(mongo_source.get_database(_db), mongo_target.get_database(_db))
     else:
         compare_collection(mongo_source.get_database(db), mongo_target.get_database(db))
+
+    exit(exit_code)
 
 
 if __name__ == '__main__':
