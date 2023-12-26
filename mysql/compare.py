b0863c9c5139d551fe3ddd75952e6fdd40e49a52
benny <b1623546466@gmail.com>
2023-12-26 20:52:57 +0800

    save

--- mysql/compare.py
+++ mysql/compare.py
@@ -63,7 +63,7 @@ def get_mysql_info(connection_string: str, ignored_db: list[str]):
             tables = inspector.get_table_names(schema=database)
             r_tables[database] = sorted(tables)
             for table in tables:
-                print(inspector.get_table_options(table))
+                print(inspector.get_table_options(table, schema=database))
                 r_indices[f'{database}-{table}'] = sorted(inspector.get_indexes(table, schema=database), key=lambda item: item['name'])
 
             # Use the connection to execute queries
